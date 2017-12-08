ebe3594536e1439199a7f993e809ac5726bdc6de
GeorgeWalker <george@walkersoftware.ca>
2017-12-08 12:32:03 -0800

    Changes to support loading home page content

--- Gov.News.Api.Client/Extensions/CategoryOperationExtensions.cs
+++ Gov.News.Api.Client/Extensions/CategoryOperationExtensions.cs
@@ -18,21 +18,47 @@ namespace Gov.News.Api
         /// </param>
 
 
-        /*
 
-        public static AzureAsset GetCategoryByKey(this ICategoryOperations operations, string key)
+        public static IDictionary<string, Post> GetTopPosts(this ICategoryOperations operations, string kind, IList<string> categories)
         {
-            return System.Threading.Tasks.Task.Factory.StartNew(s => ((IArticleOperations)s).GetCategoryByKeyAsync(key), operations, System.Threading.CancellationToken.None, System.Threading.Tasks.TaskCreationOptions.None, System.Threading.Tasks.TaskScheduler.Default).Unwrap().GetAwaiter().GetResult();
+            return System.Threading.Tasks.Task.Factory.StartNew(s => ((ICategoryOperations)s).GetTopPostsAsync(kind, categories), operations, System.Threading.CancellationToken.None, System.Threading.Tasks.TaskCreationOptions.None, System.Threading.Tasks.TaskScheduler.Default).Unwrap().GetAwaiter().GetResult();
         }
-        
-        public static async System.Threading.Tasks.Task<Category> GetCategoryByKeyAsync(this ICategoryOperations operations, string key, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        /// <param name='cancellationToken'>
+        /// The cancellation token.
+        /// </param>
+        public static async System.Threading.Tasks.Task<IDictionary<string, Post>> GetTopPostsAsync(this ICategoryOperations operations, string kind, System.Collections.Generic.IList<string> categories, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
         {
-            using (var _result = await operations.GetOneWithHttpMessagesAsync(key, ClientExtensions.currentAPIVersion, null, cancellationToken).ConfigureAwait(false))
+            using (var _result = await operations.GetTopPostsWithHttpMessagesAsync(ClientExtensions.currentAPIVersion, kind, categories, null, cancellationToken).ConfigureAwait(false))
             {
                 return _result.Body;
             }
         }
 
-    */
+
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        public static IDictionary<string, Post> GetFeaturePosts(this ICategoryOperations operations, string kind, IList<string> categories)
+        {
+            return System.Threading.Tasks.Task.Factory.StartNew(s => ((ICategoryOperations)s).GetFeaturePostsAsync(kind, categories), operations, System.Threading.CancellationToken.None, System.Threading.Tasks.TaskCreationOptions.None, System.Threading.Tasks.TaskScheduler.Default).Unwrap().GetAwaiter().GetResult();
+        }
+
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        /// <param name='cancellationToken'>
+        /// The cancellation token.
+        /// </param>
+        public static async System.Threading.Tasks.Task<IDictionary<string, Post>> GetFeaturePostsAsync(this ICategoryOperations operations, string kind, IList<string> categories, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+        {
+            using (var _result = await operations.GetFeaturePostsWithHttpMessagesAsync(ClientExtensions.currentAPIVersion, kind, categories, null, cancellationToken).ConfigureAwait(false))
+            {
+                return _result.Body;
+            }
+        }
     }
 }
