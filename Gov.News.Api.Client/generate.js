07081bb8235398e5330645d0faaf05cf79d5cb7c
André Lashley <andre.lashley@gmail.com>
2020-05-04 16:21:48 -0700

    Print node version

--- Gov.News.Api.Client/generate.js
+++ Gov.News.Api.Client/generate.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var url = "https://dev.api.news.gov.bc.ca/swagger/v1/swagger.json";
 var dest = __dirname + path.sep + "swagger.json";
 
+console.log("using node version ", process.version);
+
 var cb = function() {
 	var command = "autorest --verbose --input-file=" + __dirname + "/swagger.json --output-folder=" + __dirname + "/generated  --csharp --use-datetimeoffset --generate-empty-classes --override-client-name=Client  --namespace=Gov.News.Api";
 
